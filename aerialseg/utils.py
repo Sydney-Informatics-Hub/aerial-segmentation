fae9fbd415ec554b4de5e3367406ffdc6a7d91f8
Sahand <sahand.vahindia@sydney.edu.au>
2023-10-05 12:33:57 +1100

    WIP: added detectron batch predictor functionality
    Testing in progress. This functionality does not use the batch processing built into detectron2. It is a sequential process. Using a faster way is in #TODO.

--- aerialseg/utils.py
+++ aerialseg/utils.py
@@ -1,9 +1,17 @@
-from PIL import Image
+# -*- coding: utf-8 -*-
 import os
 
+import cv2
+import numpy as np
+import pandas as pd
+import supervision as sv
+from aerial_conversion import coco
+from PIL import Image
+from tqdm import tqdm
+
+
 def save_images_as_gif(input_folder, output_gif_path, duration=100):
-    """
-    Save a folder of images as an animated GIF.
+    """Save a folder of images as an animated GIF.
 
     Args:
     - input_folder (str): Path to the folder containing image files (e.g., JPEG or PNG).
@@ -13,7 +21,11 @@ def save_images_as_gif(input_folder, output_gif_path, duration=100):
     Returns:
     - None
     """
-    image_files = [f for f in os.listdir(input_folder) if f.endswith(('.jpg', '.png', '.jpeg', '.gif'))]
+    image_files = [
+        f
+        for f in os.listdir(input_folder)
+        if f.endswith((".jpg", ".png", ".jpeg", ".gif"))
+    ]
 
     if not image_files:
         print("No image files found in the input folder.")
@@ -26,4 +38,117 @@ def save_images_as_gif(input_folder, output_gif_path, duration=100):
         images.append(img)
 
     # Save the animated GIF
-    images[0].save(output_gif_path, save_all=True, append_images=images[1:], duration=duration, loop=0)
+    images[0].save(
+        output_gif_path,
+        save_all=True,
+        append_images=images[1:],
+        duration=duration,
+        loop=0,
+    )
+
+
+def extract_output_annotations(output, flatten: bool = False):
+    """Extracts polygons, bounding boxes, and binary masks from prediction
+    ouputs.
+
+    Args:
+        output: Detectron2 prediction output
+        flatten (bool): If true, will flatten polygons, as such used in coco segmentations.
+
+    #TODO: Simplify shapes
+    """
+    mask_array = output["instances"].pred_masks.to("cpu").numpy()
+    num_instances = mask_array.shape[0]
+    # scores = output['instances'].scores.to("cpu").numpy()
+    labels = output["instances"].pred_classes.to("cpu").numpy()
+    bbox = output["instances"].pred_boxes.to("cpu").tensor.numpy()
+    # print(mask_array.shape)
+    mask_array = np.moveaxis(mask_array, 0, -1)
+    mask_arrays = []
+    polygons = []
+    for i in range(num_instances):
+        # img = np.zeros_like(image)
+        mask_array_instance = mask_array[:, :, i : (i + 1)]
+        mask_arrays.append(mask_array_instance)
+        # img = np.where(mask_array_instance[i] == True, 255, img)
+        polygon = sv.mask_to_polygons(mask_array_instance)[0]
+        if flatten:
+            polygons.append(polygon.flatten().tolist())
+        else:
+            polygons.append(polygon.tolist())
+
+    return mask_arrays, polygons, bbox, labels
+
+
+def extract_tile_annotations_df(
+    image_path, image_id, predictor, threshold: float = 0.5
+):
+    """Reads through tiles, predicts, and extracts annnotations as a dataframe.
+
+    Args:
+        image_path (str): path to the tile png file
+        image_id (int): an id for the image tile. Usually a unique int
+        predictor: Detectron2 predictor object
+
+    #TODO: Implement thresholding
+    """
+    image = cv2.imread(image_path)
+    output = predictor(image)
+    _, polygons, _, labels = extract_output_annotations(output)
+    annotations = pd.DataFrame(
+        {"pixel_polygon": polygons, "image_id": image_id, "class_id": labels}
+    )  # "annot_id" should be added later
+
+    return annotations
+
+
+def extract_all_annotations_df(images_list: list, predictor, threshold: float = 0.5):
+    """Extract and combine tile annotations into a single dataframe.
+
+    Args:
+        images_list (list): A list of image paths
+        predictor: Detectron2 predictor object
+    """
+
+    all_annotations = []
+    for image_index, image in tqdm(enumerate(images_list), total=len(images_list)):
+        all_annotations.append(
+            extract_tile_annotations_df(
+                image, image_index, predictor, threshold=threshold
+            )
+        )
+
+    all_annotations = pd.concat(all_annotations)
+    all_annotations = all_annotations.reset_index(drop=True)
+    all_annotations = all_annotations.reset_index()
+    all_annotations.columns = ["annot_id", "pixel_polygon", "image_id", "class_id"]
+
+    return all_annotations
+
+
+def assemble_coco_json(
+    annotations, images, license: str = "", info: str = "", type: str = "instances"
+):
+    """Generate a coco json object.
+
+    Args:
+        annotations (Pandas.DataFrame): a dataframe of annotations, usually generated via extract_all_annotations_df function.
+        images (list): a list of image paths
+        license (str): license of the dataset
+        info (str): info of the dataset
+        type (str, optional): type of the segmentation. Defaults to "instances"
+    """
+    coco_json = coco.coco_json()
+    coco_json.images = coco.create_coco_images_object_png(images).images
+    coco_json.annotations = coco.coco_polygon_annotations(
+        annotations
+    )  # [tmp2]#[annots_tmp[0]]#
+    coco_json.license = license
+    coco_json.type = type
+    coco_json.info = info
+    coco_json.categories = [
+        coco.make_category(class_name=str(cat), class_id=cat)
+        for cat in annotations.groupby("class_id").groups.keys()
+    ]
+
+    return coco_json
---
+++ scripts/prediction_batch_detectron2.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import glob
+import json
+import os
+
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultPredictor
+
+from aerialseg.utils import assemble_coco_json, extract_all_annotations_df
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Run Detectron2 prediction on aerial imagery and show image with results."
+    )
+    parser.add_argument(
+        "--indir",
+        "-i",
+        type=str,
+        required=True,
+        help="Path to a directory containing the input images.",
+    )
+    parser.add_argument(
+        "--in-pattern",
+        "-p",
+        type=str,
+        default="*.png",
+        help="Glob pattern to match the input images. Default: %(default)s.",
+    )
+    parser.add_argument(
+        "--config",
+        "-c",
+        type=str,
+        required=True,
+        help="Model configuration YAML file from Detectron2.",
+    )
+    parser.add_argument(
+        "--weights",
+        "-w",
+        type=str,
+        required=True,
+        help="Path to a weights .pth file output from Detectron2 training.",
+    )
+    parser.add_argument(
+        "--threshold",
+        "-t",
+        type=float,
+        default=0.7,
+        help="Detection threshold. Default: %(default)s.",
+    )
+    parser.add_argument(
+        "--coco",
+        type=str,
+        default=None,
+        help="Path to a COCO JSON containg the annotation categories. "
+        "These will be printed onto the image if provided.",
+    )
+    parser.add_argument(
+        "--force-cpu",
+        action=argparse.BooleanOptionalAction,
+        help="If set, will force CPU inference.",
+    )
+    parser.add_argument(
+        "--coco-out",
+        "-o",
+        type=str,
+        default=None,
+        help="Path to a COCO JSON file to save the predictions to. By default will save to the same directory as the input image with 'coco-out.json' name.",
+    )
+
+    return parser
+
+
+def main(args=None):
+    parser = create_parser()
+    args = parser.parse_args()
+    cfg = get_cfg()
+
+    config_file = args.config
+    weights_file = args.weights
+    cfg.merge_from_file(config_file)
+    cfg.MODEL.WEIGHTS = weights_file
+
+    # If we have the COCO JSON then we can set up the class names for the prediction.
+    if args.coco is not None:
+        with open(args.coco, "r") as f:
+            coco = json.load(f)
+        categories = coco["categories"]
+        thing_classes = [c["name"] for c in categories]
+        meta = MetadataCatalog.get("predict")
+        meta.thing_classes = thing_classes
+
+    # scan the input directory for images based on the pattern given
+    images = glob.glob(os.path.join(args.indir, args.in_pattern))
+    assert (
+        len(images) > 0
+    ), f"No images found in the input directory given the pattern {args.in_pattern}."
+
+    # If not many images are given, this should be okay to go with CPU inference.
+    if args.force_cpu:
+        cfg.MODEL.DEVICE = "cpu"
+    else:
+        args.n_jobs = 1  # force to 1 job if using GPU
+
+    predictor = DefaultPredictor(cfg)
+
+    all_annotations = extract_all_annotations_df(
+        images, predictor
+    )  # , threshold=args.threshold)
+    coco_json = assemble_coco_json(
+        all_annotations, images, license="", info="", type="instances"
+    )
+    if args.coco_out is None:
+        args.coco_out = os.path.join(args.indir, "coco-out.json")
+
+    with open(args.coco_out, "w") as f:
+        json.dump(coco_json, f)
+
+
+if __name__ == "__main__":
+    main()
