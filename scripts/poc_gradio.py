bb94ee049ec0d6fc80fdda8a77340b647dc6d6c4
xinwei89 <113005350+xinwei89@users.noreply.github.com>
2023-10-12 11:58:13 +1100

    addressed requested changes

--- scripts/poc_gradio.py
+++ scripts/poc_gradio.py
@@ -1,41 +1,36 @@
-"""
-building-segmentation
-Proof of concept showing effectiveness of a fine tuned instance segmentation model for deteting buildings.
-"""
+# -*- coding: utf-8 -*-
+"""building-segmentation Proof of concept showing effectiveness of a fine tuned
+instance segmentation model for deteting buildings."""
 import os
-import cv2
-os.system("pip install 'git+https://github.com/facebookresearch/detectron2.git'")
-from transformers import DetrFeatureExtractor, DetrForSegmentation
-from PIL import Image
+
 import gradio as gr
 import numpy as np
-import torch
-import torchvision
-import detectron2
+from PIL import Image
 
 # import some common detectron2 utilities
-import itertools
-import seaborn as sns
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.utils.visualizer import ColorMode
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.checkpoint import DetectionCheckpointer
+if (
+    os.system("pip install 'git+https://github.com/facebookresearch/detectron2.git'")
+    == 0
+):
+    from detectron2.config import get_cfg
+    from detectron2.engine import DefaultPredictor
+    from detectron2.utils.visualizer import ColorMode, Visualizer
+else:
+    print("Failed to import detectron2 utilities")
+    exit(0)
 
 cfg = get_cfg()
 cfg.merge_from_file("model_weights/buildings_poc_cfg.yml")
-cfg.MODEL.DEVICE='cpu'
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5 
-cfg.MODEL.WEIGHTS = "model_weights/chatswood_buildings_poc.pth"  
+cfg.MODEL.DEVICE = "cpu"
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+cfg.MODEL.WEIGHTS = "model_weights/chatswood_buildings_poc.pth"
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 8
 predictor = DefaultPredictor(cfg)
 
-def segment_buildings(im):
 
-    """
-    Segment buildings in an image using a pre-trained instance segmentation model.
+def segment_buildings(im):
+    """Segment buildings in an image using a pre-trained instance segmentation
+    model.
 
     Args:
     im (PIL.Image): The input image to segment buildings from.
@@ -46,17 +41,15 @@ def segment_buildings(im):
 
     im = np.array(im)
     outputs = predictor(im)
-    v = Visualizer(im[:, :, ::-1],
-                   scale=0.5,
-                   instance_mode=ColorMode.IMAGE_BW
-    )
+    v = Visualizer(im[:, :, ::-1], scale=0.5, instance_mode=ColorMode.IMAGE_BW)
     # Print out information to appear in logs for diagnostics
-    print(len(outputs["instances"])," buildings detected.")
+    print(len(outputs["instances"]), " buildings detected.")
     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    
+
     return Image.fromarray(out.get_image()[:, :, ::-1])
 
-# gradio components 
+
+# gradio components
 """
 gr_slider_confidence = gr.inputs.Slider(0,1,.1,.7,
                                         label='Set confidence threshold % for masks')
@@ -69,7 +62,6 @@ title = "Building Segmentation"
 description = "An instance segmentation demo for identifying boundaries of buildings in aerial images using DETR (End-to-End Object Detection) model with MaskRCNN-101 backbone."
 
 # Create user interface and launch
-gr.Interface(segment_buildings, 
-                inputs = inputs,
-                outputs = outputs,
-                description = description).launch(debug=True)
+gr.Interface(
+    segment_buildings, inputs=inputs, outputs=outputs, description=description
+).launch(debug=True)
