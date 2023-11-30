5e6b03a4e2231f0f1aad41997e1830fe91147301
Sahand <sahand.vahindia@sydney.edu.au>
2023-11-30 16:25:55 +1100

    WIP modified the approach on area calcs

--- scripts/density_map.py
+++ scripts/density_map.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import logging
 
-# import geopandas as gpd
+import geopandas as gpd
+
 # import rasterio as rio
+import numpy as np
+from shapely.geometry import Polygon
 
 # set up logging
 logging.basicConfig(level=logging.INFO)
@@ -43,7 +46,7 @@ def storey_averager(annotation, storey_column="storeys"):
 
 def density_estimate_combined_area(
     annotation,
-    crs="EPSG:3857",
+    crs=None,
     average_storeys=None,
     footprint_ratio: float = 0.5,
     storey_column: str = "storeys",
@@ -55,7 +58,7 @@ def density_estimate_combined_area(
 
     Args:
         annotation (geodataframe): A geodataframe of annotations.
-        crs (str): The crs of the annotation geodataframe to calculate the area. For meters, use 'EPSG:3857'. For degrees, use 'EPSG:4326'.
+        crs (str): The crs of the annotation geodataframe to calculate the area. If none is given, will rely on UTM crs. If fails, will use 'EPSG:3857' as fallback. Defaults to None.
         average_storeys (int): The average number of storeys of buildings in the annotation geodataframe. If None, will not calculate the average number of storeys using the meta data. Defaults to None.
         footprint_ratio (float): The ratio of the footprint-area-based density to number-based density calculations. It should be a number between 0 and 1. 0 means the footprint area density won't be considered and 1 means number density won't be considered. Defaults to 0.5.
 
@@ -83,7 +86,7 @@ def density_estimate_combined_area(
 
 
 def density_estimate_number_area(
-    annotation, crs="EPSG:3857", average_storeys=None, storey_column: str = "storeys"
+    annotation, crs=None, average_storeys=None, storey_column: str = "storeys"
 ) -> float:
     """This function will get the area of annotation geodataframe, and also get
     the number of geometries in the annotation geodataframe, and return a
@@ -92,12 +95,22 @@ def density_estimate_number_area(
 
     Args:
         annotation (geodataframe): A geodataframe of annotations.
-        crs (str): The crs of the annotation geodataframe to calculate the area. For meters, use 'EPSG:3857'. For degrees, use 'EPSG:4326'.
+        crs (str): The crs of the annotation geodataframe to calculate the area. If none is given, will rely on UTM crs. If fails, will use 'EPSG:3857' as fallback. Defaults to None.
         average_storeys (int): The average number of storeys of buildings in the annotation geodataframe. If None, will not calculate the average number of storeys using the meta data. Defaults to None.
 
     Returns:
         density (float): The area of the annotation geodataframe divided by the number of geometries in the annotation geodataframe.
     """
+
+    if crs is None:
+        try:
+            crs = annotation.estimalte_utm_crs()
+        except Exception as e:
+            crs = "EPSG:3857"
+            logger.warning(
+                f"Could not estimate the UTM crs of the annotation geodataframe. Will use {crs} as fallback."
+            )
+            print(e)
 
     if annotation.crs != crs:
         annotation = annotation.to_crs(crs)
@@ -127,7 +140,7 @@ def density_estimate_number_area(
 
 
 def density_estimate_area_area(
-    annotation, crs="EPSG:3857", average_storeys=None, storey_column: str = "storeys"
+    annotation, crs=None, average_storeys=None, storey_column: str = "storeys"
 ) -> float:
     """This function will get the area of annotation geodataframe, and also get
     the number of geometries in the annotation geodataframe, and return a
@@ -136,12 +149,21 @@ def density_estimate_area_area(
 
     Args:
         annotation (geodataframe): A geodataframe of annotations.
-        crs (str): The crs of the annotation geodataframe to calculate the area. For meters, use 'EPSG:3857'. For degrees, use 'EPSG:4326'.
+        crs (str): The crs of the annotation geodataframe to calculate the area. If none is given, will rely on UTM crs. If fails, will use 'EPSG:3857' as fallback. Defaults to None.
         average_storeys (int): The average number of storeys of buildings in the annotation geodataframe. If None, will not calculate the average number of storeys using the meta data. Defaults to None.
 
     Returns:
         density (float): The area of the annotation geodataframe divided by the number of geometries in the annotation geodataframe.
     """
+    if crs is None:
+        try:
+            crs = annotation.estimalte_utm_crs()
+        except Exception as e:
+            crs = "EPSG:3857"
+            logger.warning(
+                f"Could not estimate the UTM crs of the annotation geodataframe. Will use {crs} as fallback."
+            )
+            print(e)
 
     if annotation.crs != crs:
         annotation = annotation.to_crs(crs)
@@ -173,12 +195,88 @@ def density_estimate_area_area(
     return density
 
 
-def density_map_maker(geojson, average_storeys, footprint_ratio, tile_size, map_units):
+def density_map_maker(
+    gdf,
+    average_storeys: int = None,
+    footprint_ratio: float = 0.5,
+    tile_size: int = 100,
+    size_unit: str = None,
+    area_unit: str = "utm",
+):
     """This function will use the density_estimate_combined_area function to
-    create a density map, by tiling the geojson."""
+    create a density map, by tiling the geojson.
 
-    # make a sliding window on the geojson, with the size of tile_size
-    # for each window, calculate the density_estimate_combined_area
-    # return a density map
+    Args:
+        gdf (geodataframe): A geodataframe of annotations.
+        average_storeys (int): The average number of storeys of buildings in the annotation geodataframe. If None, will not calculate the average number of storeys using the meta data. Defaults to None.
+        footprint_ratio (float): The ratio of the footprint-area-based density to number-based density calculations. It should be a number between 0 and 1. 0 means the footprint area density won't be considered and 1 means number density won't be considered. Defaults to 0.5.
+        tile_size (int): The size of the tile. Defaults to 10.
+        size_unit (str): The unit of the tile size. If is None, will use the unit of the crs of the gdf or from 'area_unit'. If set to 'percent', will use the percentage of the width of the gdf bounds. Defaults to percent. Overall, this can be ignored as long as percentage of width is the preferred window size.
+        area_unit (str): The unit of the area. Defaults to "utm".
+    """
 
-    pass
+    # Prepare the gdf
+    if area_unit == "meter":
+        crs = 3857
+        gdf = gdf.to_crs(epsg=crs)
+    elif area_unit == "utm":
+        crs = gdf.estimate_utm_crs()
+        gdf = gdf.to_crs(crs)
+    elif area_unit is None:
+        crs = None
+    else:
+        logger.warning("area_unit must be 'meter', 'utm', or None.")
+
+    # Get the bounds of the gdf
+    bounds = gdf.total_bounds
+    width = abs(bounds[2] - bounds[0])
+    height = abs(bounds[3] - bounds[1])
+    x_min, y_min, x_max, y_max = bounds
+
+    # Get the tile size
+    if size_unit == "percent":
+        tile_size = width * tile_size / 100
+    elif size_unit is None:
+        pass
+    else:
+        logger.warning("size_unit must be 'percent' or None. Will assume None.")
+
+    assert tile_size > 0, "tile_size must be greater than 0."
+
+    x_coords = np.arange(x_min, x_max, tile_size)
+    y_coords = np.arange(y_min, y_max, tile_size)
+    polygons = []
+    for x in x_coords:
+        for y in y_coords:
+            polygons.append(
+                Polygon(
+                    [
+                        (x, y),
+                        (x + tile_size, y),
+                        (x + tile_size, y + tile_size),
+                        (x, y + tile_size),
+                    ]
+                )
+            )
+    grid = gpd.GeoDataFrame({"geometry": polygons}, crs=gdf.crs)
+    intersected = gpd.overlay(gdf, grid, how="intersection")
+
+    # TODO for each window, calculate the density_estimate_combined_area
+
+    for extent in intersected.geometry:
+        raster_extent = gpd.GeoDataFrame({"id": 1, "geometry": [extent]}, crs=gdf.crs)
+        tile_polygons = gdf.clip(raster_extent)
+
+        # Split multipolygon
+        tile_polygons = tile_polygons.explode(index_parts=False)
+        tile_polygons = tile_polygons.reset_index(drop=True)
+
+        intersected
+        tile_polygons
+        average_storeys
+        footprint_ratio
+        tile_size
+        size_unit
+        height
+
+    # TODO return a density map
